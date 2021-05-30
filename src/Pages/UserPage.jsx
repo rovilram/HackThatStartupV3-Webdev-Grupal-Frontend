5061d51e0a775a836be3f86b239cfdafa3e9656c
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-05-30 20:11:35 +0200

    Use effect deleted

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -22,10 +22,6 @@ const ContentWrapper = styled.section`
 `;
 
 export const UserPage = () => {
-
-    useEffect(()=> {
-        // getUserData();
-    }, []);
     return (
         <ContentWrapper>
             <User />
